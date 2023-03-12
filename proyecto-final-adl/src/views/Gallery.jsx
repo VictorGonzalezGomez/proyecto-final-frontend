4353b431f6db0df9d8437d9bca79f2300c0e14a2
Pablo Goeppinger <pgoeppin@gmail.com>
2023-03-12 17:49:10 -0300

    feat: private views and component sidemenu added

---
+++ proyecto-final-adl/src/views/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Menu from "../components/Menu"
+
+export default function Gallery () {
+    return(
+        <div className="gallery-charts d-flex min-vh-100">
+            <Menu/>
+            {/* Mansonry https://getbootstrap.com/docs/5.3/examples/masonry/ */}
+        </div>
+    )
+}
