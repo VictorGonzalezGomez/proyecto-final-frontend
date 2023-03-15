7c1b353c9c8681768392b694c8c0a1e0dda52f9b
Pablo Goeppinger <pgoeppin@gmail.com>
2023-03-15 14:53:31 -0300

    feat: adding charts in cards in first private view

--- proyecto-final-adl/src/components/Charts.jsx
+++ proyecto-final-adl/src/components/Charts.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { ChartContext } from "../context/ChartContext";
+import { Line } from 'react-chartjs-2';
 
 export default function Charts() {
-    const { sales } = React.useContext(ChartContext)
+    const { charts } = React.useContext(ChartContext)
+    console.log({charts})
+    
     return (
-        <div className="d-flex flex-wrap gap-3 justify-content-center pt-5 container m-0">
-            {sales.map((sale) => (
-                <div key={sale.id} className="card shadow-sm" style={{width: "20rem" }}>
-                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>{sale.product_name}</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect>
-                    <text x="40%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                <div className="card-body">
-                  <p className="card-text">{sale.product_name}</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-success">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-primary">Edit</button>
-                      <button type="button" className="btn btn-sm btn-outline-danger">Delete</button>
-                    </div>
+        <div className="d-flex flex-wrap gap-3 justify-content-center p-5 m-0">
+
+          {charts[0]?.data?.length !== 0 ? charts.map((chart) => (
+            <div className="card shadow-sm" key={chart.id} style={{ width: "20rem" }}>
+                <Line data={chart.data} />
+              <div className="card-body">
+                <p className="card-text">{chart.title}</p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="btn-group">
+                    <button type="button" className="btn btn-sm btn-outline-success">View</button>
+                    <button type="button" className="btn btn-sm btn-outline-primary">Edit</button>
+                    <button type="button" className="btn btn-sm btn-outline-danger">Delete</button>
                   </div>
                 </div>
               </div>
-            ))}
-            {/* New Chart component */}
+            </div>
+          )) 
+          : <h1>Loading...</h1>}
+
         </div>
     )
 }
--- proyecto-final-adl/src/views/Gallery.jsx
+++ proyecto-final-adl/src/views/Gallery.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import Charts from "../components/Charts";
 import Menu from "../components/Menu"
-import Charts from "../components/Charts"
 export default function Gallery () {
     return(
-        <div className="gallery-charts d-flex min-vh-100">
+        <div className="gallery-charts d-flex">
             <Menu/>
             <Charts/>
         </div>
