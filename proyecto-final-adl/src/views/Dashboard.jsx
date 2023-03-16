908c42ec341cd7ec3216d3cb2dfea24524f090c3
Sherydan <tobar.luis.c@gmail.com>
2023-03-15 22:30:10 -0300

    added: gallery router and login functionallity

--- proyecto-final-adl/src/views/Dashboard.jsx
+++ proyecto-final-adl/src/views/Dashboard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import Menu from "../components/Menu";
+import swal from "sweetalert";
+import { Navigate } from "react-router-dom";
+import { AuthContex } from "../context/AuthContextProvider";
 
 export default function Dashboard () {
+    const { isAuth } = React.useContext(AuthContex);
+    if (!isAuth) {
+        swal({
+            title: "Error",
+            text: "You are not logged in",
+            icon: "error",
+            button: false,
+        });
+        setTimeout(() => {
+            swal.close();
+        }, 2000);
+
+        return <Navigate to="/" replace={true} />;
+    }
+    
     return (
         <div className="d-flex min-vh-100">
             <Menu />
--- proyecto-final-adl/src/views/Gallery.jsx
+++ proyecto-final-adl/src/views/Gallery.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Charts from "../components/Charts";
-import Menu from "../components/Menu"
-export default function Gallery () {
-    return(
+import Menu from "../components/Menu";
+import swal from "sweetalert";
+import { Navigate } from "react-router-dom";
+import { AuthContex } from "../context/AuthContextProvider";
+export default function Gallery() {
+    const { isAuth } = React.useContext(AuthContex);
+    if (!isAuth) {
+        swal({
+            title: "Error",
+            text: "You are not logged in",
+            icon: "error",
+            button: false,
+        });
+        setTimeout(() => {
+            swal.close();
+        }, 2000);
+
+        return <Navigate to="/" replace={true} />;
+    }
+    return (
         <div className="gallery-charts d-flex">
-            <Menu/>
-            <Charts/>
+            <Menu />
+            <Charts />
         </div>
-    )
+    );
 }
