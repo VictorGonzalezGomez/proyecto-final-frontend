2cf183eaaf229262b04beb823a50c2f2b81cfa0d
VictorGonzalezGomez <v.gonzalezgomez.vg@gmail.com>
2023-03-23 00:47:10 -0300

    add: join regsiter user and store in public view

--- proyecto-final-adl/src/views/store/RegisterFormStore.jsx
+++ proyecto-final-adl/src/views/store/RegisterFormStore.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import axios from "axios";
 
 const RegisterFormStore = () => {
-    const EMAIL_REGEX = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
-    const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-    const REGISTER_STORE_URL="";
+  const EMAIL_REGEX = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+  const REGISTER_STORE_URL="";
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName]= useState("");
+
 
   const [storeName, setStoreName]= useState("");
   const [rut, setRut]= useState("");
@@ -14,6 +18,8 @@ const RegisterFormStore = () => {
   const [password, setPassword]= useState("");
   const [auxPassword, setAuxPassword]= useState("");
   const [errMsg, setErrMsg] = useState("");
+
+
   const handleSubmit = async (e) => {
    
     const validateEmailFormat = EMAIL_REGEX.test(email);
@@ -35,6 +41,9 @@ const RegisterFormStore = () => {
           REGISTER_STORE_URL,
         { storeName, rut, email, industry, address, password }
       );
+    
+    setFirstName("");
+    setLastName("");
 
     setStoreName("");
     setRut("");
@@ -55,31 +64,59 @@ const RegisterFormStore = () => {
 
   return (
       
-    <div  class="d-flex align-items-center justify-content-center bg-image">
+  <div  class="d-flex align-items-center justify-content-center bg-image">
     <div class="card text-center m-5"  style={{maxWidth: '600px'}}>
-        <div class="card-body m-5 px-5">
-            <h5 class="card-title text-uppercase text-center mb-5">CREATE STORE ACCOUNT </h5>
-            <div class="row g-2">
-                <div class="col-md mb-3">
-                    <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingInputGridStoreName" placeholder="Store Name" onChange={(e)=> setStoreName(e.target.value)} />
-                        <label for="floatingInputGridStoreName">Store Name</label>
-                    </div>
+      <div class="card-body m-5 px-5">
+        <h5 class="card-title text-uppercase text-center mb-5">CREATE USER ACCOUNT</h5>
+        <div class="row g-2">
+          <div class="col-md mb-3">
+            <div class="form-floating">
+              <input type="text" class="form-control" id="floatingInputGridFirstname" placeholder="First name" onChange={(e)=> setFirstName(e.target.value)} />
+              <label for="floatingInputGridFirstname">First name</label>
+            </div>
+          </div>
+          <div class="col-md mb-3">
+            <div class="form-floating">
+              <input type="text" class="form-control" id="floatingInputGridLastName" placeholder="Last Name" onChange={(e)=> setLastName(e.target.value)}/>
+              <label for="floatingInputGridLastName">Last Name</label>
+            </div>
+          </div>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" onChange={(e)=> setEmail(e.target.value)}/>
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" onChange={(e)=> setPassword(e.target.value)}/>
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Repeat your password" onChange={(e)=> setAuxPassword(e.target.value)}/>
+          <label for="floatingPassword">Repeat your password</label>
+        </div>
+
+          <h5 class="card-title text-uppercase text-center my-5">CREATE STORE ACCOUNT </h5>
+        <div class="row g-2">
+          <div class="col-md mb-3">
+            <div class="form-floating">
+              <input type="text" class="form-control" id="floatingInputGridStoreName" placeholder="Store Name" onChange={(e)=> setStoreName(e.target.value)} />
+              <label for="floatingInputGridStoreName">Store Name</label>
+            </div>
+            </div>
+              <div class="col-md mb-3">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="floatingInputGridRut" placeholder="RUT" onChange={(e)=> setRut(e.target.value)}/>
+                  <label for="floatingInputGridRut">RUT</label>
                 </div>
-                <div class="col-md mb-3">
-                    <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingInputGridRut" placeholder="RUT" onChange={(e)=> setRut(e.target.value)}/>
-                        <label for="floatingInputGridRut">RUT</label>
-                    </div>
-                </div>
+              </div>
             </div>
             <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" onChange={(e)=> setEmail(e.target.value)}/>
-                <label for="floatingInput">Email address</label>
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" onChange={(e)=> setEmail(e.target.value)}/>
+              <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInputGridRut" placeholder="RUT" onChange={(e)=> setAddress(e.target.value)}/>
-                <label for="floatingInputGridAddress">Address</label>
+              <input type="text" class="form-control" id="floatingInputGridRut" placeholder="RUT" onChange={(e)=> setAddress(e.target.value)}/>
+              <label for="floatingInputGridAddress">Address</label>
             </div>
 
             <select class="form-select  mb-3" aria-label="SelectIndustry">
@@ -88,25 +125,16 @@ const RegisterFormStore = () => {
               <option>Food;Drink</option>
               <option>Other</option>
             </select>
-            
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" onChange={(e)=> setPassword(e.target.value)}/>
-                <label for="floatingPassword">Password</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Repeat your password" onChange={(e)=> setAuxPassword(e.target.value)}/>
-                <label for="floatingPassword">Repeat your password</label>
-            </div>
 
             <div class="d-grid gap-2">
-                <button type="button" class="fw-bold btn btn-primary btn-lg mb-3" onClick={handleSubmit}>Register</button>
+              <button type="button" class="fw-bold btn btn-primary btn-lg mb-3" onClick={handleSubmit}>Register</button>
             </div>
             <p class="card-text">Have already an account?  <a href="/" class="card-link text-reset fw-bold">Login here</a></p>
         </div>
     </div>
-    </div>
+  </div>
 
-    );
+  );
 };
 
 export default RegisterFormStore;
