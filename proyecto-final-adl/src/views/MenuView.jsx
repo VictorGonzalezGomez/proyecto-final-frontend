98e302577e3c5b5767e0122605885e9b18137bdd
Sherydan <tobar.luis.c@gmail.com>
2023-03-29 21:06:32 -0300

    fix merg into new branch

--- proyecto-final-adl/src/views/MenuView.jsx
+++ proyecto-final-adl/src/views/MenuView.jsx
@@ -2,79 +2,161 @@ import React from "react";
 import { ChartContext } from "../context/ChartContext";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
-
+import checkCsvFormat from "../helpers/checkCsvFormat";
+import swal from "sweetalert";
 import { useState } from "react";
-
 import Papa from "papaparse";
 
 export default function Menu() {
     const [file, setFile] = useState(null);
-
-    // create a loading state
-
     const [loading, setLoading] = useState(false);
 
     const handleFileChange = (event) => {
+        // check if file is empty
+        if (event.target.files[0] === null) {
+            return;
+        }
         setFile(event.target.files[0]);
     };
 
     const handleSubmit = async () => {
-        // create a loading state
         setLoading(true);
-        const csv = await convertToCSV(file);
-        const jsonData = await convertToJSON(csv);
-        await sendData(jsonData);
+        try {
+            if (file === null) {
+                swal({
+                    title: "Error",
+                    text: "Please select a file",
+                    icon: "error",
+                    button: "Ok",
+                });
+                setLoading(false);
+                return;
+            }
+            const csv = await convertToCSV(file);
+            const jsonData = await convertToJSON(csv);
+            // check formart
+            if (!checkCsvFormat(csv)) {
+                swal({
+                    title: "Error",
+                    text: "Please select a csv file in the correct format",
+                    icon: "error",
+                    button: "Ok",
+                });
+                setLoading(false);
+                return;
+            }
+            await sendData(jsonData);
+        } catch (error) {
+            console.log(error);
+        }
         setLoading(false);
+        setFile(null);
     };
 
     const convertToCSV = (file) => {
-        return new Promise((resolve, reject) => {
-            Papa.parse(file, {
-                complete: (results) => {
-                    resolve(results.data);
-                    console.log(results.data);
-                },
-                error: (error) => {
-                    reject(error);
-                },
+        // check if file is empty
+        if (file === null) {
+            return;
+        }
+
+        // check if file is csv
+        if (file.type !== "text/csv") {
+            swal({
+                title: "Error",
+                text: "Please select a csv file",
+                icon: "error",
+                button: "Ok",
             });
-        });
+            return;
+        }
+
+        try {
+            return new Promise((resolve, reject) => {
+                Papa.parse(file, {
+                    complete: (results) => {
+                        resolve(results.data);
+                        console.log(results.data);
+                    },
+                    error: (error) => {
+                        reject(error);
+                    },
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     const convertToJSON = (csv) => {
-        // Aquí deberás escribir la función que convierta el CSV a JSON
-        const headers = csv[0];
-        const jsonData = [];
-
-        for (let i = 1; i < csv.length; i++) {
-            const row = csv[i];
-            const obj = {};
-
-            for (let j = 0; j < headers.length; j++) {
-                obj[headers[j]] = row[j];
-            }
-
-            jsonData.push(obj);
+        //check if csv is empty
+        if (csv.length === 0) {
+            return;
         }
-        // delete last row because its full of undefined values
-        jsonData.pop();
+        try {
+            const headers = csv[0];
+            const jsonData = [];
 
-        console.log(jsonData);
+            for (let i = 1; i < csv.length; i++) {
+                const row = csv[i];
+                const obj = {};
 
-        return jsonData;
+                for (let j = 0; j < headers.length; j++) {
+                    obj[headers[j]] = row[j];
+                }
+
+                jsonData.push(obj);
+            }
+            // delete last row because its full of undefined values
+            jsonData.pop();
+
+            return jsonData;
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     // send data function
 
     const sendData = async (jsonData) => {
-        await axios
-            .post("http://localhost:3000/sales", jsonData)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
+        let sendingError = false;
+        try {
+            await axios
+                .post("http://localhost:3000/sales", jsonData)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    swal({
+                        title: "Error",
+                        text: "Error while uploading data",
+                        icon: "error",
+                        button: "Ok",
+                    });
+                    console.log(error);
+                    sendingError = true;
+                });
+        } catch (error) {
+            console.log(error);
+        }
+
+        if (!sendingError) {
+            swal({
+                title: "Success",
+                text: "Data uploaded successfully",
+                icon: "success",
+                button: "Ok",
             });
+        } else {
+            swal({
+                title: "Error",
+                text: "Error while uploading data",
+                icon: "error",
+                button: "Ok",
+            });
+        }
+
+        // close modal after sending data
+        // x = e => { document.getElementById('closeModal').click() }
     };
 
     const { charts } = React.useContext(ChartContext);
@@ -201,9 +283,12 @@ export default function Menu() {
                                 aria-label="Close"
                             ></button>
                         </div>
-                        <div className="modal-body">
+                        <div className="modal-body mx-auto">
                             {loading ? (
-                                <div className="spinner-border" role="status">
+                                <div
+                                    className="spinner-border text-primary"
+                                    role="status"
+                                >
                                     <span className="visually-hidden">
                                         Loading...
                                     </span>
@@ -228,20 +313,26 @@ export default function Menu() {
                             )}
                         </div>
                         <div className="modal-footer">
-                            <button
-                                type="button"
-                                className="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                            >
-                                Close
-                            </button>
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                onClick={handleSubmit}
-                            >
-                                Send Sales
-                            </button>
+                            {loading && null}
+                            {!loading && (
+                                <>
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        data-bs-dismiss="modal"
+                                        id="closeModal"
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-primary"
+                                        onClick={handleSubmit}
+                                    >
+                                        Send Sales
+                                    </button>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
